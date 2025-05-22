54b1fa2a4f764a6c240da5c4a6c1a418513683f8
Dilshan Perera <83137780+dilshanperera98@users.noreply.github.com>
2025-05-22 13:56:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from langchain_experimental.sql import SQLDatabaseChain
 # Load API keys and DB URI
 load_dotenv()  
 openai_api_key = os.getenv("OPENAI_API_KEY")  
-mysql_uri = os.getenv("MYSQL_URI")   sdvsdvsdv 
+mysql_uri = os.getenv("MYSQL_URI")   sdvsdvsd
    
 # Connect to the MySQL database      
 db = SQLDatabase.from_uri(mysql_uri)
